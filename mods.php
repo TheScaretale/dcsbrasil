1ea419b28d9ac05f92833aa6cd0b7674c6a13295
Mikael <mikaelrsgomes@gmail.com>
2024-03-20 16:07:49 -0300

    kkkkkkkkkkkkkkkkkkkk

---
+++ mods.php
@@ -0,0 +1,56 @@
+<?php include_once 'nav.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DCSBrasil.net(DEV)</title> 
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;800&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <style>
+        body{
+            background-color: black;
+        }
+        .indented {
+            text-indent: 400px; /* Adjust the value as needed */
+        }
+    </style>
+</head>
+<body>
+    <main>
+        <section class="py-5 text-center container">
+            <div class="row py-lg-5">
+                <div class="col-lg-6 col-md-8 mx-auto">
+                    <h4 class="text-center" style="color: white;">Desde a versão <span style="color: red;">2.9.3.51704</span> não existe mais a versão "Open Beta" do DCS.</h4>
+                </div>
+            </div>
+        </section>
+        <div class="album py-5 bg-secondary text-light">
+            <div class="container">
+                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                    <div class="col">
+                        <img src="assets/img/mods_location2.png" alt="">
+                        <br><br>
+                        <img src="assets/img/mods_location.png" alt="">
+                    </div>
+                    <div class="col">
+                        <p> Um erro comum quando alguns tentam instalar os MODS Free, é instalar na pasta errada. Muitos acabam tentando instalar na pasta MODS do próprio SIMULADOR e assim não funcionará!<br>
+                            <br>
+                            Os Arquivos tem que ser salvos na pasta MODS do seu Saved Games (Jogos Salvos) que é nativa do seu Windows:<br>
+                            <span class="indented">Exemplo: C:\Users\XXX\Saved Games\DCS.openbeta\Mods ou<br>
+                            C:\Usuários\XXX\Jogos Salvos\DCS.openbeta\Mods</span><br>
+                            <br>
+                            <span class="indented">"XXX" É o seu nome de usuário do windows!</span><br>
+                            <br>
+                            Lembrando de se atentar em qual pasta colocar cada MOD, normalmente nos Manuais vem escrito. (⬅️ Exemplo na Imagem ao lado)
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,80 @@
+<nav class="navbar navbar-expand-lg" data-bs-theme="dark">
+        <div class="container-fluid justify-content-center">
+             <a href="" class="navbar-brand">DCSBrasil.net</a>
+             <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                 <span class="navbar-toggler-icon"></span>
+             </button>
+             <div class="collapse navbar-collapse flex-grow-0" id="navbarSupportedContent">
+                 <ul class="navbar-nav me-auto mb-2 mb-lg-0 text-center">
+                     <div>
+                         <li class="nav-item">
+                             <a href="index.html" class="nav-link" aria-current="page">Home</a>
+                         </li>
+                     </div>
+                     <div>
+                         <li class="nav-item dropdown">
+                             <a href="" class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">Wild Pigeons</a>
+                             <ul class="dropdown-menu">
+                                 <li class="dropdown-item">Roster</li>
+                                 <li class="dropdown-item">COMMANDS/COMANDOS</li>
+                             </ul>
+                         </li>
+                     </div>
+                     <div>
+                         <li class="nav-item">
+                             <a href="#" class="nav-link">Pontuação</a>
+                         </li>
+                     </div>
+                     <div>
+                         <li class="nav-item">
+                             <a href="mods.html" class="nav-link  active">Mods</a>
+                         </li>
+                     </div>
+                     <div>
+                         <li class="nav-item">
+                             <a href="https://www.digitalcombatsimulator.com/en/personal/profile/" class="nav-link">Perfil do DCS</a>
+                         </li>
+                     </div>
+                     <div>
+                         <li class="nav-item">
+                             <a href="https://www.digitalcombatsimulator.com/en/" class="nav-link">Site Oficial do DCS</a>
+                         </li>
+                     </div>
+                     <div>
+                         <li class="nav-item">
+                             <a href="https://www.digitalcombatsimulator.com/en/news/changelog/" class="nav-link">Ultimo changelog</a>
+                         </li>
+                     </div>
+                     <div>
+                         <li class="nav-item dropdown">
+                             <a href="" class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">Iniciando no DCS</a>
+                             <ul class="dropdown-menu">
+                                 <li class="dropdown-item">Links Úteis</li>
+                                 <li class="dropdown-item">Fraseologia</li>
+                                 <li class="dropdown-item">Operações Embarcadas</li>
+                                 <li class="dropdown-item">Tutoriais</li>
+                                 <li class="dropdown-item">Tasks</li>
+                                 <li class="dropdown-item">Servidores</li>
+                                 <li class="dropdown-item">Kneeboards</li>
+                             </ul>
+                         </li>
+                     </div>
+                     <div>
+                         <li class="nav-item dropdown">
+                             <a href="" class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">Missões</a>
+                             <ul class="dropdown-menu">
+                                 <li class="dropdown-item">#01 Ataque Coordenado</li>
+                                 <li class="dropdown-item">#02 Escolta C-130</li>
+                                 <li class="dropdown-item">#03 Vietnam</li>
+                                 <li class="dropdown-item">#04 Defend UKR</li>
+                                 <li class="dropdown-item">#05 Operação Louva-Deus</li>
+                                 <li class="dropdown-item">#06 Operação Opera</li>
+                                 <li class="dropdown-item">AH-64 #1 - EN</li>
+                                 <li class="dropdown-item">AH-64 #2 - PT/BR</li>
+                             </ul>
+                         </li>
+                     </div>
+                 </ul>
+             </div>
+         </div>
+     </nav>
